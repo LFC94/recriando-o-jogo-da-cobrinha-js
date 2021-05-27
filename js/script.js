ad4d10d5c5d3425597fd1ddd954a1b060423577b
Lucas Felipe Costa <lucasfelipecost4@hotmail.com>
2021-05-27 10:38:47 -0300

    Criando o snake

--- js/script.js
+++ js/script.js
@@ -2,10 +2,22 @@ var can_snake = document.getElementById('can_snake');
 var context = can_snake.getContext("2d");
 var box = 32;
 var snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+};
 
 function criarBG() {
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
+function criarSnake() {
+   for (let index = 0; index < snake.length; index++) {
+       context.fillStyle = "green";
+       context.fillRect(snake[index].x, snake[index].y, box, box);
+   }
+}
+
 criarBG();
+criarSnake();
